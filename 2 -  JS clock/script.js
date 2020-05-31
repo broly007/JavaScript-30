c26399c11bbc4ca8c63fa952220fc58951cd1612
broly007 <kumarhritik158@gmail.com>
2020-05-31 16:32:57 +0530

    JS clock

---
+++ 2 -  JS clock/script.js
@@ -0,0 +1,29 @@
+
+
+
+const secondHand = document.querySelector('.second-hand')
+const minuteHand = document.querySelector('.minute-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds()   
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes/60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 60) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+        
+}
+setDate();
+setInterval(setDate, 1000);
+
+
+
+
+
